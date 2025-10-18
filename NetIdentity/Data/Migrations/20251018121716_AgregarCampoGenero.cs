3c55eb40f11d15052285b2f520cae260af7f27c0
Chucho <willyguevara048@gmail.com>
2025-10-18 08:23:54 -0400

    Agregar el campo Genero

---
+++ NetIdentity/Data/Migrations/20251018121716_AgregarCampoGenero.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetIdentity.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarCampoGenero : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Genero",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Genero",
+                table: "AspNetUsers");
+        }
+    }
+}
--- NetIdentity/Models/ApplicationUser.cs
+++ NetIdentity/Models/ApplicationUser.cs
@@ -2,11 +2,19 @@
 
 namespace NetIdentity.Models
 {
+    public enum Genero
+    {
+        Masculino,
+        Femenino,
+        Otro,
+        PrefieroNoDecirlo
+    }
+
     public class ApplicationUser : IdentityUser
     {
         public DateTime FechaNacimiento { get; set; }
         public string? NombreCompleto { get; set; }
-        public string genero { get; set; } //Masculino | Femenino | Otro | GeneroX | GeneroY ; M | F | O | X | Y
+        public Genero Genero { get; set; } //Masculino | Femenino | Otro | GeneroX | GeneroY ; M | F | O | X | Y
         //public bool isFemenino? (1) =0(masculino) | isMasculino 1|0 
     }
 
